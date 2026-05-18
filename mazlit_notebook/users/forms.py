b329715248205636b8502704ef90a55300d58b19
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2026-05-18 11:54:26 +1000

    Add form to login view

---
+++ mazlit_notebook/users/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput)
+    password = forms.CharField(widget=forms.PasswordInput)
--- mazlit_notebook/users/views.py
+++ mazlit_notebook/users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import LoginForm
 
 # Create your views here.
+def index(request):
+    pass
+
+def login_view(request):
+    form = LoginForm()
+    
+    return render(request, 'users/login.html', {'form': form})
+
+def logout_view(request):
+    pass
