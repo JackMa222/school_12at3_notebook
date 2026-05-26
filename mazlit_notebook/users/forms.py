86dad1f9e4f7addd8b7137fc4b59c1e3f81365f4
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2026-05-26 11:48:48 +1000

    Add login and logout feature to auth

--- mazlit_notebook/users/forms.py
+++ mazlit_notebook/users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
@@ -10,3 +11,33 @@ class LoginForm(AuthenticationForm):
         'class': 'input input-bordered w-full',
         'placeholder': '••••••••'
     }))
+    
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
+        'class': 'input input-bordered w-full',
+        'placeholder': 'name@example.com'
+    }))
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['username'].widget = forms.TextInput(attrs={
+            'class': 'input input-bordered w-full',
+            'placeholder': 'Choose a username'
+        })
+        
+        self.fields['password1'].widget = forms.PasswordInput(attrs={
+            'class': 'input input-bordered w-full',
+            'placeholder': '••••••••'
+        })
+        self.fields['password2'].widget = forms.PasswordInput(attrs={
+            'class': 'input input-bordered w-full',
+            'placeholder': 'Confirm your password'
+        })
+        
+        self.fields['password1'].label = "Password"
+        self.fields['password2'].label = "Confirm Password"
--- mazlit_notebook/users/views.py
+++ mazlit_notebook/users/views.py
@@ -1,14 +1,54 @@
-from django.shortcuts import render
-from .forms import LoginForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import LoginForm, RegisterForm
 
 # Create your views here.
+@login_required
 def index(request):
-    pass
+    return render(request, 'users/index.html')
 
 def login_view(request):
-    form = LoginForm()
+    if request.user.is_authenticated:
+        return redirect('index')
     
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Welcome back, {username}")
+                return redirect("index")
+            else:
+                messages.error(request, "Invalid username or password.")
+    else:
+        form = LoginForm()
+ 
     return render(request, 'users/login.html', {'form': form})
 
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    
+    if request.method == 'POST':
+        form = RegisterForm(request, data=request.POST)
+        if form.is_valid:
+            user = form.save()
+            login(request, user)
+            
+            messages.success(request, f"Account created successfully.")
+    
+    form = RegisterForm()
+    
+    return render(request, 'users/register.html', {'form': form})
+
 def logout_view(request):
-    pass
+    logout(request)
+    
+    return redirect('login')
