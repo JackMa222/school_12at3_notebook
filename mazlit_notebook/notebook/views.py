04646c001d295421c78d77a2d5a321994a8af905
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2026-06-01 19:57:45 +1000

    Add user to created organiser entry to DB

--- mazlit_notebook/notebook/views.py
+++ mazlit_notebook/notebook/views.py
@@ -17,7 +17,10 @@ def organisers(request):
         if form.is_valid():
             organiser_name = form.cleaned_data.get('organiser_name')
             
-            new_organiser = Organiser.objects.create(name=organiser_name)
+            new_organiser = Organiser.objects.create(
+                name=organiser_name,
+                user=request.user
+            )
             
             messages.success(request, f"Organiser '{organiser_name}' created successfully")
             return redirect("notebook:organisers")
