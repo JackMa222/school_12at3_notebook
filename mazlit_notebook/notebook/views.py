4bc7148f4ad843f2e36bd5aabc1e4180096db7e0
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2026-06-01 19:45:35 +1000

    Add messages as component on all pages

--- mazlit_notebook/notebook/views.py
+++ mazlit_notebook/notebook/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import OrganiserForm
+from .models import Organiser
 
 # Create your views here.
 @login_required
@@ -9,6 +11,17 @@ def index(request):
 
 @login_required
 def organisers(request):
+    if request.method == "POST":
+        form = OrganiserForm(request.POST)
+        
+        if form.is_valid():
+            organiser_name = form.cleaned_data.get('organiser_name')
+            
+            new_organiser = Organiser.objects.create(name=organiser_name)
+            
+            messages.success(request, f"Organiser '{organiser_name}' created successfully")
+            return redirect("notebook:organisers")
+    
     form = OrganiserForm()
     
     return render(request, 'notebook/organisers.html', {'form': form})
