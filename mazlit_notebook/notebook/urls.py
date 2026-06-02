9bc235c6a38e0e75ea00946cff4b78eda7a42f3f
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2026-06-02 20:14:31 +1000

    Add version footnote to sidebar nav

--- mazlit_notebook/notebook/urls.py
+++ mazlit_notebook/notebook/urls.py
@@ -6,7 +6,7 @@ app_name = 'notebook'
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("organisers", views.OrganiserCreateListView.as_view(), name="organisers"),
+    path("organisers/", views.OrganiserCreateListView.as_view(), name="organisers"),
     path("organisers/<int:pk>/", views.OrganiserUpdateView.as_view(), name='organiser_info'),
     path('organisers/<int:pk>/delete/', views.OrganiserDeleteView.as_view(), name='organiser_delete'),
     path("payment_bodies/", views.PaymentBodyCreateListView.as_view(), name='pymt_bodies'),
